3fc487c25f0948e12ae13c1b9c8415ea87162f82
yangarbiter <yangarbiter@users.noreply.github.com>
2019-12-11 15:08:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from mkdir_p import mkdir_p
 def main():
     mkdir_p("./results/exp_name")
     auto_var.register_experiment('exp_name', run_exp_name,
-                                 {'result_file_dir': './results/exp_name'})
-    auto_var.parse_argparse(
+                                 {'file_format': 'pickle', 'result_file_dir': './results/exp_name'})
+    auto_var.parse_argparse()
 
 
 if __name__ == '__main__':
