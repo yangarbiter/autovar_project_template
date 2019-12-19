b95d2b0ab66c68ec60f12102104f8f98f180fc45
yangarbiter <yangarbiter@users.noreply.github.com>
2019-12-18 23:07:32 -0800

    Update params.py

--- params.py
+++ params.py
@@ -13,38 +13,13 @@ class sample_experiments(SampleExperiments):
         cls.grid_params = grid_params
         return SampleExperiments.__new__(cls, *args, **kwargs)
 
-class compare_algos_const(StreamingExperiments):
+class Sample01Experiment(SampleExperiments):
     def __new__(cls, *args, **kwargs):
-        cls.name = "compare_algos"
+        cls.name = "Sample01"
         grid_params = []
         grid_params.append({
-            'learner': [f'const-biooja-{10**i}' for i in range(1, 5)] + [f'const-oja-{10**i}' for i in range(4, 8)],
-            'dataset': ['synthetic00-10000-10'],
             'random_seed': random_seed,
         })
         cls.grid_params = grid_params
-        return StreamingExperiments.__new__(cls, *args, **kwargs)
+        return SampleExperiments.__new__(cls, *args, **kwargs)
 
-class compare_algos_01(StreamingExperiments):
-    def __new__(cls, *args, **kwargs):
-        cls.name = "compare_algos"
-        grid_params = []
-        grid_params.append({
-            'learner': [f'biooja-{10**i}' for i in range(1, 8)] + [f'oja-{10**i}' for i in range(1, 8)],
-            'dataset': ['synthetic01-10000-10'],
-            'random_seed': random_seed,
-        })
-        cls.grid_params = grid_params
-        return StreamingExperiments.__new__(cls, *args, **kwargs)
-
-class compare_algos_const_01(StreamingExperiments):
-    def __new__(cls, *args, **kwargs):
-        cls.name = "compare_algos"
-        grid_params = []
-        grid_params.append({
-            'learner': [f'const-biooja-{10**i}' for i in range(1, 5)] + [f'const-oja-{10**i}' for i in range(4, 8)],
-            'dataset': ['synthetic01-10000-10'],
-            'random_seed': random_seed,
-        })
-        cls.grid_params = grid_params
-        return StreamingExperiments.__new__(cls, *args, **kwargs)
