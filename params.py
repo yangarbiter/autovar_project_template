340e2114a289c0de1565a43063feb1ee86dd21a7
yangarbiter <yangarbiter@users.noreply.github.com>
2019-12-18 23:11:38 -0800

    Update params.py

--- params.py
+++ params.py
@@ -12,14 +12,3 @@ class sample_experiments(SampleExperiments):
         })
         cls.grid_params = grid_params
         return SampleExperiments.__new__(cls, *args, **kwargs)
-
-class Sample01Experiment(SampleExperiments):
-    def __new__(cls, *args, **kwargs):
-        cls.name = "Sample01"
-        grid_params = []
-        grid_params.append({
-            'random_seed': random_seed,
-        })
-        cls.grid_params = grid_params
-        return SampleExperiments.__new__(cls, *args, **kwargs)
-
