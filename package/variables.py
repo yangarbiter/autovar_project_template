0575f387c55271856c44d81b735952cded9ce5db
yangarbiter <yangarbiter@users.noreply.github.com>
2020-05-01 16:12:20 -0700

    Update variables.py

--- package/variables.py
+++ package/variables.py
@@ -1,16 +1,25 @@
 import logging
 from functools import partial
 
+import numpy as np
 from autovar import AutoVar
+from autovar.base import RegisteringChoiceType, register_var, VariableClass
 from autovar.hooks import check_result_file_exist, save_result_to_file
+from autovar.hooks import create_placeholder_file, remove_placeholder_if_error
 from autovar.hooks import default_get_file_name as get_file_name
 
 from .datasets import DatasetVarClass
 
 auto_var = AutoVar(
     logging_level=logging.INFO,
-    before_experiment_hooks=[partial(check_result_file_exist, get_name_fn=get_file_name)],
-    after_experiment_hooks=[partial(save_result_to_file, get_name_fn=get_file_name)],
+    before_experiment_hooks=[
+        partial(check_result_file_exist, get_name_fn=get_file_name),
+        partial(create_placeholder_file, get_name_fn=get_file_name),
+    ],
+    after_experiment_hooks=[
+         partial(save_result_to_file, get_name_fn=get_file_name),
+         partial(remove_placeholder_if_error, get_name_fn=get_file_name),
+    ],
     settings={
         'file_format': 'pickle',
         'server_url': '',
