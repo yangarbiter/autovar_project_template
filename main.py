b715add4cfd86030d04cae99086f3feb66688c4e
yangarbiter <yangarbiter@users.noreply.github.com>
2020-08-06 14:57:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from package.variables import auto_var
 from utils import setup_experiments
 
 
-def run_exp_name(auto_var):
-    pass
-
-
 def main():
     setup_experiments(auto_var)
     auto_var.parse_argparse()
